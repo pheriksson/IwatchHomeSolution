38032c97d0efed833f63095211c8fd5bc5134830
Robin Olofsson <Robin19972012@hotmail.se>
2021-02-18 17:45:14 +0100

    HealthRateView

--- Group8Application/Group8Application WatchKit Extension/ContentView.swift
+++ Group8Application/Group8Application WatchKit Extension/ContentView.swift
@@ -10,30 +10,9 @@ import HealthKit
 
 
 
-private func updateUIFromStatistic(statisticsCollection: HKStatisticsCollection){
-    
-    let startDate = Calendar.current.date(byAdding: .day,value: -7, to: Date())!
-    let endDate = Date()
-    
-    statisticsCollection.enumerateStatistics(from: startDate, to: endDate) { (statistics,stop) in
-        
-        let count = statistics.sumQuantity()?.doubleValue(for: .count())
-        
-        let step = Step(count: Int(count ?? 0), date: statistics.startDate)
-        steps.append(step)
-    }
-}
-
 struct ContentView: View {
     
-    private var healthStore: HealthStore?
-    private var homeKit: Fibaro?
-    @State private var steps: [Step] = [Step]()
-    
-    init() {
-        healthStore = HealthStore()
-        //homeKit = Fibaro()
-    }
+
     
     var body: some View {
         
@@ -43,21 +22,7 @@ struct ContentView: View {
                 Text("button")
             }
         }
-        
-        .onAppear{
-            if let healthStore = healthStore {
-                print("unwrappat healthStore")
-                healthStore.requestAuthorization { success in
-                    if success {
-                       healthStore.calculateSteps { statisticsCollection in
-                            if let statisticsCollection = statisticsCollection {
-                                updateUIFromStatistic(statisticsCollection: statisticsCollection)
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    
     }
 }
 
