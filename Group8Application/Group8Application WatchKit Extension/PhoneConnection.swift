68bb7cf38fb9d0f75f2f9f19e8cad33e5bdc7094
pheriksson <phi.eriksson@gmail.com>
2021-02-28 16:55:19 +0100

    forgot to check the commit, just pushed the new files, code for files added...

--- Group8Application/Group8Application WatchKit Extension/PhoneConnection.swift
+++ Group8Application/Group8Application WatchKit Extension/PhoneConnection.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import WatchConnectivity
+
+
+class PhoneConnection : NSObject, WCSessionDelegate{
+    
+    var session : WCSession!
+
+    override init(){
+        super.init()
+        if WCSession.isSupported(){
+            self.session = WCSession.default
+            self.session.delegate = self
+            self.session.activate()
+        }
+    }
+
+
+
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        if let notificationMsg = message["NOTIFICATION"]{
+            //Notification recieved.
+        }
+        if let testMsg = message["MSG"]{
+            print("Recieved test msg on watch: \(testMsg)")
+        }
+    }
+
+
+    func send(reqType : String, msg : String){
+        let message = [ reqType : msg]
+        session.sendMessage(message, replyHandler: nil, errorHandler: {
+            error in
+            print(error.localizedDescription)
+        })
+
+    }
+    //To be implemented.
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+
+}
+
+
