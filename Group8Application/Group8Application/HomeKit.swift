99e1e95965b66e00b64368ab40da4c07c150360c
pheriksson <phi.eriksson@gmail.com>
2021-02-28 17:03:31 +0100

    Created base method for GET requests to fibaro and updated event names from wf

--- Group8Application/Group8Application/HomeKit.swift
+++ Group8Application/Group8Application/HomeKit.swift
@@ -8,9 +8,13 @@
 import Foundation
 import HomeKit
 import SwiftUI
+
 class Fibaro: MQTTObserver{
     var id : Int
     weak var observing : MQTTClient?
+    
+    private var encoding : String?
+    private var url_base : String?
     
     struct Post: Codable , Identifiable{
         let id = UUID()
@@ -20,16 +24,21 @@ class Fibaro: MQTTObserver{
     
     var access: HMHomeManager?
     
-    init(){
+    init(_ usr : String,_ pw : String,_ ip : String){
         if (HMHomeManager.accessibilityActivate()) {
             access = HMHomeManager()
             print("fibaro stuff")
         }
         self.id = Int.random(in: 1..<9999)
         
+        //Fibaro base config.
+        self.encoding = "\(usr):\(pw)".data(using: .utf8)!.base64EncodedString()
+        self.url_base = "http://\(ip)/api/"
+        
+        
     }
     
-    init(observe: MQTTClient){
+    init(_ usr : String,_ pw : String,_ ip : String, observe: MQTTClient){
         if (HMHomeManager.accessibilityActivate()) {
             access = HMHomeManager()
         }
@@ -37,21 +46,32 @@ class Fibaro: MQTTObserver{
         self.id = Int.random(in: 1..<9999)
         self.observing = observe
         self.observing!.registerObserver(obs: self)
-
+        
+        //Fibaro base config.
+        self.encoding = "\(usr):\(pw)".data(using: .utf8)!.base64EncodedString()
+        self.url_base = "http://\(ip)/api/"
+        
+    }
+    
+    private func setupGetRequest(task : String) -> URLRequest{
+        let url = URL(string : "\(url_base!)\(task)")
+        let auth = "Basic \(encoding!)"
+        
+        var request = URLRequest(url : url!)
+        request.setValue(auth, forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        return request
     }
     
     func test(){
         
-        guard let url = URL(string : "http://130.240.114.44/api/devices/") else {return}
-        var request = URLRequest(url: url)
-        let encoding = "unicorn@ltu.se:jSCN47bC".data(using: .utf8)!.base64EncodedString()
-        let auth = "Basic \(encoding)"
-        request.setValue(auth , forHTTPHeaderField: "Authorization")
-        request.httpMethod = "GET"
+        
+        let request = setupGetRequest(task: "devices/?id=30")
         
         let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
             guard let data = data else {return}
-            print(response)
+            let str = String(decoding: data, as: UTF8.self)
+            print(str)
         }
         task.resume()
         
@@ -59,20 +79,25 @@ class Fibaro: MQTTObserver{
         
     }
     
+    
     //For responding to events from widefind, ie. code = "lämna kök" or something -> call for fibaro to turn off binary switches.
     //Todo: enum klass för alla event -> vad som ska bli kallat
     func moveEvent(code: String) {
         switch code{
-            case "ankomst lägenhet":
+            case "entering appartement":
                 print("call func 1")
-            case "lämnar lägenhet":
+            case "leaving appartement":
                 print("call func 2")
-            case "ankomst sovrum":
+            case "entering bedroom":
                 print("call func 3")
-            case "lämnar sovrum":
+            case "leaving bedroom":
                 print("call func 4")
+            case "entering kitchen":
+                print("call func 5")
+            case "leaving kitchen":
+                print("call func 6")
             default:
-                print("I feel a disturbance in the force.")
+                print("I feel a disturbance in the force, event: [\(code)] was called.")
         }
         
     }
