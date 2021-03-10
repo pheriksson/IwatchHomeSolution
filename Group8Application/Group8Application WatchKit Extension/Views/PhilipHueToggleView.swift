e3892eeeea3dbef1321650f5038f4db973023511
pheriksson <phi.eriksson@gmail.com>
2021-03-10 13:23:14 +0100

    Philip Hue view operational.

--- Group8Application/Group8Application WatchKit Extension/Views/PhilipHueToggleView.swift
+++ Group8Application/Group8Application WatchKit Extension/Views/PhilipHueToggleView.swift
@@ -8,13 +8,69 @@
 import SwiftUI
 
 struct PhilipHueToggleView: View {
+    
+    @State var isClicked: Bool
+    private var lightId : String
+    private var WMC : PhoneConnection
+    private var firstClick : prepLoad
+    
+    init(WMC : PhoneConnection, id : String, onOff : Int){
+        self.lightId = id
+        self.WMC = WMC
+        self.firstClick = prepLoad()
+        if onOff == 1{
+            _isClicked = State(initialValue: true)
+        }else{
+            _isClicked = State(initialValue: false)
+        }
+    }
+    
+    func toggleClicked(status : Bool) -> String{
+        print("toggleClicked with status: \(status)")
+        print("And light id of \(self.lightId)")
+        print("Preparing call to turn off/on light")
+        
+        return ""
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Toggle(isOn : self.$isClicked){
+            HStack{
+                Text("\(lightId)").padding()
+                if (!self.firstClick.getLoaded()){
+                    Text("\(self.firstClick.setFinishedLoading())")
+                }else{
+                    if (self.isClicked){
+                        Text("\(toggleClicked(status : true))")
+                    }else{
+                        Text("\(toggleClicked(status : false))")
+                    }
+                }
+            }
+        }
     }
 }
 
+class prepLoad{
+    
+    var prep: Bool = false
+    
+    func setFinishedLoading() -> String{
+        self.prep = true
+        return ""
+    }
+    
+    func getLoaded() -> Bool{
+        return self.prep
+    }
+    
+    
+}
+
+/*
 struct PhilipHueToggleView_Previews: PreviewProvider {
     static var previews: some View {
         PhilipHueToggleView()
     }
 }
+*/
--- Group8Application/Group8Application WatchKit Extension/Views/PhilipHueView.swift
+++ Group8Application/Group8Application WatchKit Extension/Views/PhilipHueView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct PhilipHueView: View {
+    
+    @ObservedObject var lights : HueContainer
+    var WMC : PhoneConnection
+    
+    init(phoneCon : PhoneConnection){
+        self.WMC = phoneCon
+        self.lights = WMC.getHueContainer()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if(lights.getHueLightStatus()){
+            Text("Lights recieved.")
+            let recievedLights = lights.getHueLights()
+            ForEach(recievedLights.keys.sorted(), id: \.self) { key in
+                    HStack {
+                        //Text(key)
+                        //Text("\(recievedLights[key]!)")
+                        PhilipHueToggleView(WMC: self.WMC,id : key, onOff: recievedLights[key]!, firstClick : [true])
+                    }
+                }
+            
+        }else{
+            Text("Retrieving lights from PhilipHue")
+            Image(systemName: "hourglass")
+        }
+        
     }
 }
+
+/*
 
 struct PhilipHueView_Previews: PreviewProvider {
     static var previews: some View {
         PhilipHueView()
     }
 }
+*/
